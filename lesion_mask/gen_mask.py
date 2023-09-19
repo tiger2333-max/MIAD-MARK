4621275325b8b94a2f4693f79f096eea919fe425
tiger2333-max <64149376+tiger2333-max@users.noreply.github.com>
2023-09-19 16:09:52 +0800

    Update gen_mask.py

--- lesion_mask/gen_mask.py
+++ lesion_mask/gen_mask.py
@@ -17,11 +17,14 @@ layer_names = {
 
 if __name__=='__main__':
 
-    dataset = args.dataset
+      dataset = args.dataset
     root_path = args.root_path
     model_name = args.model_name
     cla_num = args.cla_num
     seed = args.seed
+    down_val = args.down_val
+    up_val = args.up_val
+    step= args.step
     layer_name = layer_names[dataset][model_name]
 
     mask_dir=os.path.join(root_path, 'lesion_mask', 'results', dataset, model_name, layer_name)
@@ -35,6 +38,6 @@ if __name__=='__main__':
     for mask in mask_list:
         mask_img=cv2.imread(os.path.join(mask_dir,mask),0)
         print('mask_img',mask)
-        for threshold in range(110,200,20):
+        for threshold in range(down_val,up_val,step):
             ret, mask_new = cv2.threshold(mask_img, threshold, 255, cv2.THRESH_BINARY)
-            cv2.imwrite(os.path.join(save_path,os.path.splitext(mask)[0]+'_threshold_'+str(threshold)+'.png'),mask_new)
+            cv2.imwrite(os.path.join(save_path,os.path.splitext(mask)[0]+'_threshold_'+str(threshold)+'.png'),mask_new)+'.png'),mask_new)
